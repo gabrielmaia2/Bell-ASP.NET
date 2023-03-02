cd3f151728a1ba68fe5a8c56ab1f3216f2120f3a
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2023-03-02 19:17:03 -0300

    chore(seller-specs): Delete ProductRepositoryMock

--- Bell.Seller.Specs/AddProduct.cs
+++ Bell.Seller.Specs/AddProduct.cs
@@ -2,23 +2,13 @@ using Bell.Seller.Controllers;
 using Bell.Seller.Domain.Models;
 using Bell.Seller.Domain.Repositories;
 using Bell.Seller.Services;
-using Bell.Seller.Specs.Mocks;
 
 namespace Bell.Seller.Specs;
 
 public class AddProduct
 {
-    IProductRepository repository;
-
-    ProductService service;
-
-    ProductController controller;
-
     public AddProduct()
     {
-        repository = new ProductRepositoryMock(new List<Product>());
-        service = new ProductService(repository);
-        controller = new(service);
     }
 
     [Fact]
